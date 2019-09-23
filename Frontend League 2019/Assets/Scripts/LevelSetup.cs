4963d92ec4718fcef393f72b8e07d692abf77b2e
Michał Przyszczypkowski <mprzyszczypkowski@apptension.com>
2019-09-23 18:03:04 +0200

    Level init script

---
+++ Frontend League 2019/Assets/Scripts/LevelSetup.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSetup : MonoBehaviour
+{
+    public void SetupScene(int levelNumber) => Debug.Log($"Level {levelNumber} has been loaded");
+}
