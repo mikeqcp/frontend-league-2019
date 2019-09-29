bdc680175cb061bd9c3b49e7aeabfe536e4f73d5
Rafał Gruszecki <rgruszecki@apptension.com>
2019-09-29 22:23:33 +0200

    Add example shaders and gravity for concept presentation

--- Frontend League 2019/slides/concept/src/presentation.js
+++ Frontend League 2019/slides/concept/src/presentation.js
@@ -1,5 +1,5 @@
 // Import React
-import React from 'react';
+import React from "react";
 
 // Import Spectacle Core tags
 import {
@@ -12,29 +12,35 @@ import {
   Quote,
   Slide,
   Text,
-  Image,
-} from 'spectacle';
+  Image
+} from "spectacle";
 
 // Import theme
-import createTheme from 'spectacle/lib/themes/default';
+import createTheme from "spectacle/lib/themes/default";
 
 const images = {
-  mechanics: require('./assets/mechanics.png'),
+  mechanics: require("./assets/mechanics.png"),
+  earth: require("./assets/earth.jpg"),
+  mars: require("./assets/mars.jpg"),
+  meteor: require("./assets/meteor.jpg"),
+  explosion: require("./assets/explosion.gif"),
+  gravity: require("./assets/gravity.gif"),
+  space: require("./assets/space.jpg")
 };
 
 // Require CSS
-require('normalize.css');
+require("normalize.css");
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quaternary: '#CECECE',
+    primary: "white",
+    secondary: "#1F2022",
+    tertiary: "#03A9FC",
+    quaternary: "#CECECE"
   },
   {
-    primary: 'Montserrat',
-    secondary: 'Helvetica',
+    primary: "Montserrat",
+    secondary: "Helvetica"
   }
 );
 
@@ -42,11 +48,11 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
-        transition={['zoom', 'slide']}
+        transition={["zoom", "slide"]}
         transitionDuration={500}
         theme={theme}
       >
-        <Slide transition={['fade']} bgColor="primary">
+        <Slide transition={["fade"]} bgColor="primary">
           <Heading size={2} caps lineHeight={2} textColor="secondary">
             Zespół
           </Heading>
@@ -59,7 +65,7 @@ export default class Presentation extends React.Component {
             <ListItem>Jakub Wiśniewski</ListItem>
           </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
+        <Slide transition={["fade"]} bgColor="primary">
           <Heading size={2} fit caps lineHeight={2} textColor="secondary">
             Technologie & Platformy
           </Heading>
@@ -71,39 +77,75 @@ export default class Presentation extends React.Component {
             <ListItem>Android</ListItem>
           </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
+        <Slide transition={["fade"]} bgColor="primary">
           <Heading size={1} textColor="secondary" caps>
             Gameplay
           </Heading>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
+        <Slide transition={["fade"]} bgColor="tertiary">
           <Heading size={5} textColor="secondary" caps>
             Angry Birds w kosmosie
           </Heading>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
+        <Slide transition={["fade"]} bgColor="tertiary">
           <List>
             <ListItem>Strzał pociskiem do celu</ListItem>
             <ListItem>Na pocisk działają siły grawitacji planet</ListItem>
             <ListItem>Celem jest znalezienie optymalnego toru lotu</ListItem>
           </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
+        <Slide transition={["fade"]} bgColor="primary">
           <Image src={images.mechanics} />
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={2} textColor="secondary" caps>
+            Grawitacja planet
+          </Heading>
+          <Image src={images.gravity} />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={1} textColor="secondary" caps>
+            Przykładowe shadery
+          </Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={2} textColor="secondary" caps>
+            Planety
+          </Heading>
+          <Image src={images.earth} display="inline-block" width="50%"/>
+          <Image src={images.mars}  display="inline-block" width="50%"/>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={2} textColor="secondary" caps>
+            Pociski
+          </Heading>
+          <Image src={images.meteor} />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={2} textColor="secondary" caps>
+            Tło
+          </Heading>
+          <Image src={images.space} />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={2} textColor="secondary" caps>
+            Eksplozja planety
+          </Heading>
+          <Image src={images.explosion} />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
           <Heading size={1} textColor="secondary" caps>
             Features
           </Heading>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
+        <Slide transition={["fade"]} bgColor="tertiary">
           <List>
             <ListItem>Wsparcie dla AR i myszy/klawiatury</ListItem>
             <ListItem>Symulacja grawitacji w układzie planetarnym</ListItem>
             <ListItem>Animacje wybuchu planet</ListItem>
           </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
+        <Slide transition={["fade"]} bgColor="tertiary">
           <Heading size={6} textColor="primary" caps>
             Typography
           </Heading>
@@ -126,7 +168,7 @@ export default class Presentation extends React.Component {
             Standard text
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>
             Standard List
           </Heading>
@@ -137,7 +179,7 @@ export default class Presentation extends React.Component {
             <ListItem>Item 4</ListItem>
           </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <BlockQuote>
             <Quote>Example Quote</Quote>
             <Cite>Author</Cite>
